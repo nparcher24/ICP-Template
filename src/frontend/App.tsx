acd9cdbba75d7db6b11fb538ff503ff912f526f0
HIC P <nicholas.parrish@mac.com>
2022-04-27 08:46:40 -0400

    Small tweaks

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,26 +1,13 @@
 import React from "react"
 import { Intro } from "./Intro"
 import { About } from "./About"
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <Intro />
-          </Route>
-        </Switch>
+      <div className="w-full h-screen text-6xl font-bold text-center pt-24">
+        Hello World
       </div>
     </Router>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  purge: [
-    "./index.html", 
-    "./src/**/*.{js,ts,jsx,tsx}"
+  content: [
+    // Example content paths...
+    "./public/**/*.html",
+    "./src/**/*.{js,jsx,ts,tsx,vue}",
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
